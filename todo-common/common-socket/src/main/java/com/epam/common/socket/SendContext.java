dfb707c51c9da5bc35eb19fff06bcd406468c688
ford_ji <ford_ji@epam.com>
2022-08-01 16:35:34 +0800

    1. implementation send context

--- todo-common/common-socket/src/main/java/com/epam/common/socket/SendContext.java
+++ todo-common/common-socket/src/main/java/com/epam/common/socket/SendContext.java
@@ -1,4 +1,17 @@
 package com.epam.common.socket;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 public class SendContext {
+
+    private final ConcurrentMap<String, Object> ctx = new ConcurrentHashMap<>();
+
+    public Object put(final String key, final Object value) {
+        return this.ctx.put(key, value);
+    }
+
+    public Object get(final String key) {
+        return this.ctx.get(key);
+    }
 }
