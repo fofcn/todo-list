9f9558cd37bbb8004e1e4d9d5a6e1be80dfff242
ford_ji <ford_ji@epam.com>
2022-08-10 15:21:39 +0800

    1. modified JwtUtils's getPayload method for adapting Istio Envoy Filter to get raw token from Authorization HTTP header

--- todo-common/common-web/src/main/java/com/epam/todo/common/web/util/JwtUtils.java
+++ todo-common/common-web/src/main/java/com/epam/todo/common/web/util/JwtUtils.java
@@ -5,6 +5,10 @@ import com.alibaba.fastjson.JSONObject;
 import com.epam.common.core.ResponseCode;
 import com.epam.common.core.constant.SecurityConstants;
 import com.epam.common.core.exception.TodoException;
+import com.nimbusds.jose.JWSObject;
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -12,19 +16,36 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+
 
 public class JwtUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+
+    private JwtUtils() {
+
+    }
+
     public static JSONObject getJwtPayload() {
         JSONObject jsonObject = null;
-        String payload = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader(SecurityConstants.JWT_PAYLOAD_KEY);
-        if (StringUtils.hasLength(payload)) {
-            try {
-                jsonObject = JSON.parseObject(URLDecoder.decode(payload, StandardCharsets.UTF_8.name()));
-            } catch (UnsupportedEncodingException e) {
-                throw new TodoException(ResponseCode.AUTHORIZED_ERROR);
-            }
+        String token = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader(SecurityConstants.AUTHORIZATION_KEY);
+        if (!StringUtils.hasLength(token) || !StringUtils.startsWithIgnoreCase(token, SecurityConstants.JWT_PREFIX)) {
+            throw new TodoException(ResponseCode.AUTHORIZED_ERROR);
         }
+
+        token = StringUtils.replace(token, SecurityConstants.JWT_PREFIX, Strings.EMPTY);
+        LOGGER.info("Get token from request header, token is: [{}]", token);
+        try {
+            String payload = String.valueOf(JWSObject.parse(token).getPayload());
+            LOGGER.info("Get jwt payload from request header, payload is: [{}]", payload);
+            if (StringUtils.hasLength(payload)) {
+                jsonObject = JSON.parseObject(URLDecoder.decode(payload, StandardCharsets.UTF_8.name()));
+            }
+        } catch (UnsupportedEncodingException | ParseException e) {
+            throw new TodoException(ResponseCode.AUTHORIZED_ERROR);
+        }
+
         return jsonObject;
     }
 
