4391060951628b2296b9d2ce405288921561c7e9
ford_ji <ford_ji@epam.com>
2022-08-17 12:49:41 +0800

    1. fixed jpa @CreateDate @LastModifiedDate can not auto generate

--- todo-common/common-crypto/src/main/java/com/epam/common/encrypto/symmetric/AESCrypto.java
+++ todo-common/common-crypto/src/main/java/com/epam/common/encrypto/symmetric/AESCrypto.java
@@ -1,7 +1,23 @@
 package com.epam.common.encrypto.symmetric;
 
+import javax.crypto.SecretKey;
+import java.security.spec.AlgorithmParameterSpec;
+
 public class AESCrypto extends AbstractSymmetricCrypto {
-    public AESCrypto() {
+
+    public AESCrypto(SymmetricAlgorithm algorithm) {
         super(algorithm);
+    }
+
+    public AESCrypto(SymmetricAlgorithm algorithm, byte[] key) {
+        super(algorithm, key);
+    }
+
+    public AESCrypto(SymmetricAlgorithm algorithm, SecretKey secretKey) {
+        super(algorithm, secretKey);
+    }
+
+    public AESCrypto(SymmetricAlgorithm algorithm, SecretKey key, AlgorithmParameterSpec paramsSpec) {
+        super(algorithm, key, paramsSpec);
     }
 }
