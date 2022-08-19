8170ffb613567a631c9472f6b320293774f7c99a
ford_ji <ford_ji@epam.com>
2022-08-19 18:02:36 +0800

    1. check parameter validity

--- todo-common/common-crypto/src/main/java/com/epam/common/encrypto/asymmetric/AbstractAsymmetricCrypto.java
+++ todo-common/common-crypto/src/main/java/com/epam/common/encrypto/asymmetric/AbstractAsymmetricCrypto.java
@@ -1,13 +1,19 @@
 package com.epam.common.encrypto.asymmetric;
 
+import com.epam.common.core.lang.Assert;
+
 public class AbstractAsymmetricCrypto implements AsymmetricCrypto {
     @Override
     public byte[] decrypt(byte[] bytes, KeyType keyType) {
+        Assert.isNotNull(bytes, () -> new IllegalArgumentException("bytes"));
+        Assert.isNotNull(keyType, () -> new IllegalArgumentException("keyType"));
         return new byte[0];
     }
 
     @Override
     public byte[] encrypt(byte[] bytes, KeyType keyType) {
+        Assert.isNotNull(bytes, () -> new IllegalArgumentException("bytes"));
+        Assert.isNotNull(keyType, () -> new IllegalArgumentException("keyType"));
         return new byte[0];
     }
 }
